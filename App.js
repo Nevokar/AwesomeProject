24d4b96e2b77337bf2272d4420c402ebcf9dc6dd
Tinkuuu <tinkuuu@hotmail.com>
2023-09-04 17:53:32 +0300

    adding React Navigation

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import Map from "./components/Map";
 import { PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <Map />
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider>
+        <Map />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
